6d0f9a3df0dc6780fa55e0365c8297959fced9fb
Kanha Sharma <kanhasharma55527@gmail.com>
2026-03-21 12:11:51 +0530

    converstion

---
+++ conversion.js
@@ -0,0 +1,14 @@
+let score = "kanha"
+//console.log(typeof score);
+
+let valueIsNumber = Number(score);
+//console.log(valueIsNumber);
+
+let IsLoggedIn = " ";
+BooleanIsLoggedIn = Boolean(IsLoggedIn);
+//console.log(BooleanIsLoggedIn);
+
+let YourNumber = 7;
+let convertYourNumber = String(YourNumber);
+//console.log(typeof convertYourNumber);
+//console.log(convertYourNumber);
--- tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-let accountcity = "delhi";
-// console.log(accountcity);

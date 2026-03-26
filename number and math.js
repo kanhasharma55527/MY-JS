109c9dfe25dde3ae64a0392d79be6a54aa735fca
Kanha Sharma <kanhasharma55527@gmail.com>
2026-03-26 22:16:40 +0530

    number and math.js

---
+++ number and math.js
@@ -0,0 +1,36 @@
+const number = 100;
+// console.log(number);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// const balance = new Number(100);
+// console.log(balance.toString().typeof);
+
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+
+// const balance = new Number(100);
+// console.log(balance.toFixed(4));
+
+const otherNumber = 388.7773;
+// console.log(otherNumber.toPrecision(5));
+
+const hunderad = 10000;
+console.log(hunderad.toLocaleString(4));
+
+//++++++++++++++++++++++++++++++++++++++ math ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.76));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(4.78));
+// console.log(Math.min(3,6,8,9));
+// console.log(Math.max(2,4,7,9));
+// console.log(Math.random());
+// console.log(Math.random()*7);
+
+const max = 10;
+const min = 8;
+console.log(Math.floor(Math.random() * (max - min + 1)+min));

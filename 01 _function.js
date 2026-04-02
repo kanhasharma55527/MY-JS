9330067a43754fddc587e5ed04081970b356d632
Kanha Sharma <kanhasharma55527@gmail.com>
2026-04-02 19:58:05 +0530

    01_function.js

--- 01 _function.js
+++ 01 _function.js
@@ -20,12 +20,48 @@
 // console.log("result",result);
 
 
-function loginUserMessage(username){
-    if(!username)(username === undefined){
-        console.log("please enetr a username");
-        return 
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username){
+//     if(!username)(username === undefined){
+//         console.log("please enetr a username");
+//         return 
+//     }
+//     return `${username} just logged in`
+// }
 
-console.log(loginUserMessage("kanha"));
+// console.log(loginUserMessage("kanha"));
+
+// ++++++++++++++++++++++++++++++++++ fumction with object ++++++++++++++++++++++++++++++++++
+
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+// console.log(`the value of array is`,calculateCartPrice(200,300,400));
+
+// function calculateCartPrice(val1,val2, ...num1){
+//     return num1;
+//  }
+//  console.log(calculateCartPrice(200,300,400));
+
+// const user = {
+//     username:"kanha sharma",
+//     price:"199"
+// }
+
+// function handleobject(anybody) {
+//     console.log(`username is ${anybody.user} and the price is ${anybody.price}`);
+// }
+// handleobject(user);
+
+// handleobject({
+//     username:"kanha sharma",
+//     price:"200"
+// })
+
+// const myArray = [200,300,400,500];
+
+// function myNewArray(getArray){
+//     return getArray;
+// }
+
+// // console.log(myNewArray(myArray))
+// console.log(myNewArray([200,300,400,500]));

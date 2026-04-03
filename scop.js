3e72eeda5ef222e52a7e2fdd2c3252570b14fea2
Kanha Sharma <kanhasharma55527@gmail.com>
2026-04-04 00:01:44 +0530

    scop.js

--- scop.js
+++ scop.js
@@ -6,15 +6,53 @@
 // console.log(b);
 // console.log(c);
 
-var c = 100;
-if(true){
+// var c = 100;
+// if(true){
 
-let a = 10;
-const b = 20;
-var c = 30;
+// let a = 10;
+// const b = 20;
+// var c = 30;
 
-}
+// }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+// function one(){
+//     const username = "kanha"
+    
+//     function two(){
+//           const courcename = "youtube"
+//           console.log(username);
+
+//     }
+    
+//     console.log(two);
+//     two();
+// }
+
+// one();
+
+// if(true){
+//     const username = "kanha"
+//     if(username === "kanha"){
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+//     console.log(website)
+// }
+// console.log(username);
+
+console.log(addon(5));
+
+function addon(num){
+    return num;
+}
+
+
+console.log(addtwo(5));
+const addtwo = function(num){
+    return num + 2;
+}
+addtwo(5);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-// let a = 10;
-// const b = 20;
-// var c = 30;
+console.log(username);

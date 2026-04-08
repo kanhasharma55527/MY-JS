7fff35d936200532d91d57ea9fec9907537203a8
Kanha Sharma <kanhasharma55527@gmail.com>
2026-04-08 19:18:34 +0530

    _while.js

---
+++ _while.js
@@ -0,0 +1,12 @@
+// let i = 0;
+// while(i <= 10){
+//     console.log(`value of i is ${i}`);
+//     i = i + 1;
+// }
+
+let myArray = ["ironman","spiderman","capatain america","thor"];
+let arr = 0;
+while(arr < myArray.length){
+    console.log(`value of arr is ${myArray[arr]}`);
+    arr++;
+}

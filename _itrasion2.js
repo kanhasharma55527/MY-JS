261ed2a77b25f169894bf6415902748f486924f8
Kanha Sharma <kanhasharma55527@gmail.com>
2026-04-11 10:59:25 +0530

    _itrasion.js

---
+++ _itrasion2.js
@@ -0,0 +1,49 @@
+// const coding = ["js","python","java","c++"];
+// const value = coding.forEach((item) => {
+//     console.log(item);
+//     return item;
+// });
+
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10];
+// const newNums = myNums.filter((num) => num > 4);
+// console.log(newNums);
+
+
+
+// const books = [
+//   { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//   { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//   { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//   { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//   { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//   { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//   { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//   { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//   { title: 'Book Nine', genre: 'History', publish: 2005, edition: 2015 } // ✅ important
+// ];
+
+// let userBooks = books.filter((bk) => 
+//   bk.publish >= 2000 && bk.genre === "History"
+// );
+
+// console.log(userBooks);
+
+// +++++++++++++++++++map++++++++++++++++++++++
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10];
+// const newNums = myNums.map((num) => num + 10)
+//                       .map((num) => num  + 1)
+//                       .filter((num) => num > 40);
+// console.log(newNums);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+const newNums = myNums
+  .map((num) => num + 30)
+  .map((num) => num + 20)
+  .filter((num) => num > 40);
+
+console.log(newNums);
